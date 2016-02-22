634ff44cdeb836f292f07eefc077ca641d94e3fd
nOraKat147 <noribuying@gmail.com>
2016-02-22 09:02:21 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,43 @@
-# Enter your procedural solution here!
+def is_prime(int)
+  if int <= 1
+    return false
+    elsif
+      int <= 3
+      return true
+  end#if
+
+  i = 2
+  while i*i <= int do
+    if int % i == 0
+      return false
+    end
+    i += 1
+  end#while
+  return true
+end
+
+def largest_prime_factor(int)
+  arr = Array.new
+  for i in 0..int
+    if is_prime(i)
+      arr.push(i)
+    end
+  end
+  prime_factors = Array.new
+  arr.each do |i|
+    if int % i == 0
+      prime_factors.push(i)
+    end
+  end
+  return prime_factors[-1]
+end
+
+
+=begin
+for i in 0..10
+  if is_prime(i)
+  p "i: " + i.to_s + " " + is_prime(i).inspect
+  end
+end
+=end
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,42 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(int)
+    @int = int
+  end
+
+  def is_prime(int)
+    if int <= 1
+      return false
+      elsif
+        int <= 3
+        return true
+    end#if
+
+    i = 2
+    while i*i <= int do
+      if int % i == 0
+        return false
+      end
+      i += 1
+    end#while
+    return true
+  end
+
+  def number
+    arr = Array.new
+    for i in 0..@int
+      if is_prime(i)
+        arr.push(i)
+      end
+    end
+    prime_factors = Array.new
+    arr.each do |i|
+      if @int % i == 0
+        prime_factors.push(i)
+      end
+    end
+    return prime_factors[-1]
+  end
+
+end
